e04e481fb7ca469502183a069863ed4db4be75b6
AliyevVusal <vusalma@code.edu.az>
2022-04-27 14:38:23 +0400

    beta

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-AOS.init();
-
 
 // Get the modal
 var modal = document.getElementById("pcmyModal");
@@ -29,39 +27,48 @@ window.onclick = function(event) {
 
 
 
-// // Get the modal
-// var mobilmodal = document.getElementById("myModal");
+// Get the modal
+var mobilmodal = document.getElementById("myModal");
 
-// // Get the button that opens the modal
-// var mobilbtn = document.getElementById("myBtn");
+// Get the button that opens the modal
+var mobilbtn = document.getElementById("myBtn");
 
-// // Get the <span> element that closes the modal
-// var mobilspan = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var mobilspan = document.getElementsByClassName("close")[0];
 
-// // When the user clicks on the button, open the modal
-// mobilbtn.onclick = function() {
-//   mobilmodal.style.display = "block";
-// }
+// When the user clicks on the button, open the modal
+mobilbtn.onclick = function() {
+  mobilmodal.style.display = "block";
+}
 
-// // When the user clicks on <span> (x), close the modal
-// mobilspan.onclick = function() {
-//   mobilmodal.style.display = "none";
-// }
+// When the user clicks on <span> (x), close the modal
+mobilspan.onclick = function() {
+  mobilmodal.style.display = "none";
+}
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == mobilmodal) {
-//     mobilmodal.style.display = "none";
-//   }
-// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == mobilmodal) {
+    mobilmodal.style.display = "none";
+  }
+}
+
 
 //Side Bar
 let menu = document.querySelector("#menu");
 let sidebar = document.querySelector("#sidebar");
-//deyerler gelir scss problemidi
+let sidebarclose = document.querySelector("#sidebarclose")
 
 menu.addEventListener('click' , () => {
-  sidebar.classList.toggle('sidebaractive');
+  sidebar.style.visibility = "visible"
+  sidebar.style.opacity = "1"
+  sidebar.style.width = "100%"
+})
+
+sidebarclose.addEventListener('click',()=>{
+  sidebar.style.visibility = "hidden"
+  sidebar.style.opacity = "0"
+  sidebar.style.width = "0%"
 })
 
 
@@ -100,27 +107,12 @@ $(document).ready(function(){
     $(".moneychange").slideToggle();
   })
 
-  $(".bplus").click(function(){
-    $("#beverages").slideToggle();
-  })
-
-  $(".brplus").click(function(){
-    $("#breads").slideToggle();
-  })
-
-  $(".breplus").click(function(){
-    $("#breakfast").slideToggle();
-  })
-
-  $(".vgplus").click(function(){
-    $("#Vegetables").slideToggle();
-  })
-
-  $(".mplus").click(function(){
-    $("#Meats").slideToggle();
-  })
-
 });
+
+
+
+
+
 
 
 
